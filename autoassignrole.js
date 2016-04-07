7a109a1ffb26e11995a5f16aa946e9cac73b80b1
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-04-07 17:14:51 -0400

    Fix user registration and form element.

--- autoassignrole.js
+++ autoassignrole.js
@@ -1,4 +1,15 @@
 /**
+ * Given a autoassignrole variable name from Drupal (excluding the "autoassignrole_" prefix), this will return that
+ * variable's value or null if it doesn't exist.
+ * @param key
+ * @returns {null}
+ */
+function autoassignrole_config(key) {
+  return typeof drupalgap.site_settings['autoassignrole_' + key] !== 'undefined' ?
+      drupalgap.site_settings['autoassignrole_' + key] : null;
+}
+
+/**
  * Implements hook_services_success().
  */
 function autoassignrole_services_postprocess(options, data) {
@@ -13,19 +24,60 @@ function autoassignrole_services_postprocess(options, data) {
 }
 
 /**
+ * Preprocess a service call.
+ * @param {Object} options
+ */
+function autoassignrole_services_preprocess(options) {
+  try {
+    // Remove any 0 values from the user_roles data during user registration.
+    if (options.service == 'user' && options.resource == 'register') {
+      var data = JSON.parse(options.data);
+      if (data.user_roles) {
+        var user_roles = {};
+        for (var rid in data.user_roles) {
+          if (data.user_roles[rid]) {
+            user_roles[rid] = rid;
+          }
+        }
+        data.user_roles = user_roles;
+        options.data = JSON.stringify(data);
+      }
+    }
+  }
+  catch (error) { console.log('autoassignrole_services_preprocess - ' + error); }
+}
+
+/**
  * Implements hook_form_alter().
  */
 function autoassignrole_form_alter(form, form_state, form_id) {
+
+  // Add the user roles input to the user registration form.
   if (form_id == 'user_register_form') {
-    form.elements['user_roles'] = {
-      type: 'checkboxes',
-      title: 'Select Your Role(s)',
-      options: {}
+
+    // Try to determine an appropriate weight for the element.
+    var weight = 0;
+    if (form.elements.submit) {
+      if (typeof form.elements.submit.weight === 'undefined') { form.elements.submit.weight = 999; }
+      weight = form.elements.submit.weight - 1;
+    }
+
+    // Build the element and attach to the form.
+    var name = 'user_roles';
+    var input = {
+      type: autoassignrole_config('user_multiple') == "1" ? 'checkboxes' : 'select',
+      title: autoassignrole_config('user_title'),
+      description: autoassignrole_config('user_description').value,
+      required: autoassignrole_config('user_required') == "1",
+      options: {},
+      weight: weight
     };
     $.each(drupalgap.autoassignrole.autoassignrole_user_roles, function(rid, value) {
       if (!value) { return; }
-      form.elements.user_roles.options[rid] = value;
+      input.options[rid] = value;
     });
-  }
-}
+    form.elements[name] = input;
 
+  }
+
+}
